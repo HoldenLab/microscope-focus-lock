efa5fbde12a381c152742b57e8b93c20efa36e5f
seamusholden <seamus.holden@ncl.ac.uk>
2018-08-30 14:26:52 +0100

    First commit. Import written library

---
+++ README.md
@@ -0,0 +1,46 @@
+# Arduino-based microscope autofocus control
+## Adds in-sample offset shift to reflection autofocus systems such as the ASI CRISP
+
+##Items required
+###Electronics:
+- ASI crisp
+- Arduino Uno
+- Adafruit motor shield v2.3
+- 12V stepping motor, Unipolar Hybrid Stepper Motor 1.8°, 0.10Nm, 12 V dc, 400 mA, 6 Wires RS Stock No. 191-8299
+- PCB headers
+- 12V power supply: Plugtop PSU UK unregulated 3-12Vdc 6W RS Stock no.: 6158880
+- Pulley: MXL Plastic Pulley with insert teeth 18 RS Stock no.: 7784714
+- 2x MXL Rubber Timing Belt W1/4, L 12.00 in.  RS Stock no.: 7785089
+- Mini breadboard
+- Wires
+- 2x push switches
+###Optics & Optomechanics
+- 1” F=100mm biconvex lens (NIR coated)
+- SM1 Zoom Housing for Ø1" Optics, Non-Rotating, 2" (50.8 mm) Travel SM1NR1
+- Zoom housing ? cage adaptors:
+- 2x LCP02/M
+- SM1T10 SM1 (1.035"-40) Coupler, External Threads, 1" Long
+- Motor ? ½” post adaptor: CP06F/M
+- 20 cm of cage rods
+- CRISP mounted on cage plate adaptor:
+- LCP01/M
+- SM2A6
+- C Mount to SM1 thread adaptor.
+
+## Introduction
+We use a reflection based ASI CRISP to maintain focus on our microscope. Similar to many other systems, it reflects an IR beam off a coverslip, and uses shifts in the position of this beam to maintain constant objective-slide separation by closed loop control. 
+The limitation of this approach is that this approach only allows you to lock focus at/ very near to the coverslip, in a range < 1 um for a 100x high NA objective, or optionally, to offset the lock position a fixed amount by physically displacing the autofocus device. We wanted to image cells at different heights within the same sample, so this approach was not sufficient for us. 
+In our case the autofocus system on our custom microscope has its own beam path and tube lens, before the fluorescence path, separated by a dichroic short pass. Because the autofocus has its own tube lens we were able to produce large changes in the lock position by implementing a motorized Arduino controlled tube lens on the autofocus beam path. This effectively offsets the lock position (the coverslide) and the camera/ excitation focus by a variable amount, in our case allowing ~ 10 um of lock range on 100x NA 1.49 TIRF objective, instead of the standard <1 um with a fixed autofocus tube lens.
+Very briefly, we mounted a 100mm focal length lens on a zoom lens housing and motorized the zoom lens with an Arduino and a stepper motor.
+Code, some photos, and brief notes are supplied to help others use the same approach.
+Although this implementation is built around an ASI CRISP, in principle it should work for any commercial or home built autofocus system. 
+
+##Notes:
+- The Thorlabs zoom lens housing is controlled with a stepper motor, connected via a pulley and timing belt. Bizarrely, I could not find an easy out of the box way of adding pulley teeth to a cylindrical object (the zoom housing). As our application was low torque, I took the very low tech approach of chopping up a second timing belt, and gluing it inside out onto the outside of the zoom housing. The glued timing belt then acts as a makeshift pulley. 
+- Wire the motor to the Adafruit control board in 6-wire configuration.
+- Ensure to connect an external 12V power supply to the Arduino board to supply sufficient power to the motor.
+- Code is included to control the stepper motor and microscope focus via Arduino. Download the Arduino IDE, and open the Arduino focus-controller.ino sketch. Install the _Adafruit\_Motor\_Shield\_V2\_Library_ via the _Sketch\>Include Library\>Manage Libraries_ interface. Upload the code to the Arduino board.
+- Connect to the Arduino board and control the motor using the Arduino (or any other) serial monitor interface.
+- In addition to serial monitor control, I also added a very basic UP/ DOWN  manual control with two push buttons wired to Arduino digital I/O pins.
+- Axial magnification is proportional to the square of the lateral magnification. In order for a tube lens shift of several mm to cause sufficient focal shift, we need to use a shorter focal length tube lens for the autofocus, compared to the imaging tube lens. In our case, the (Nikon) imaging tube lens is f=200mm, and the autofocus is f=100mm, reducing the axial autofocus magnification by a factor of 4 – this gives us an ~10 um lock range.
+
---
+++ focus-controller/focus-controller.ino
@@ -0,0 +1,287 @@
+/* 
+Control stepper motor (defined as 200 steps/rev). 
+Control via commands in the Arduino Serial Monitor (or other serial interface).
+Move to absolute position. 
+Saves current position, speed to memory & loads on startup.
+Does not account for physical slippage of motor or object - need to 
+implement fancy hardware limit switches etc for that
+170724 Add a move up move down buttons
+
+Device commands:
+move val - moves device to val
+speed val - sets speed to val (valid #s 0 - 255
+pos? - returns current device position
+speed? - return current device speed
+setzero - sets current position as zero
+stepsize
+stepsize?
+home
+setmax
+setmin
+poslim   
+*/
+
+
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+#include <SoftwareSerial.h>   // We need this even if we're not using a SoftwareSerial object
+                              // Due to the way the Arduino IDE compiles
+#include <SerialCommand.h>
+#include <EEPROM.h>
+#define ADDRESS_POS 0
+#define ADDRESS_SPEED 2
+#define ADDRESS_MINPOS 3
+#define ADDRESS_MAXPOS 5
+#define STEP_PER_REV 200
+#define MIN_STORE_CHANGE 100
+
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+// Connect a stepper motor with 200 steps per revolution (1.8 degree)
+// to motor port #2 (M3 and M4)
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(STEP_PER_REV, 2);
+int curPos,eepromPos;
+byte curSpeed;
+int stepSize = 5;
+int  val; // Data received from the serial port
+int maxPos,minPos;
+SerialCommand SCmd;   // The demo SerialCommand object
+const int kPinButton1 = 2;
+const int kPinButton2 = 3;
+const unsigned long kEEPROM_update_time = 5*1000;//10s
+unsigned long timeCheck;
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Stepper connected");
+
+  EEPROM.get(ADDRESS_POS, curPos);
+  eepromPos = curPos;
+  Serial.println("Current position:");
+  Serial.println(curPos, DEC);    //This may print 'ovf, nan' if the data inside the EEPROM is not a valid float.
+  EEPROM.get(ADDRESS_SPEED, curSpeed);
+  Serial.println("Current speed:");
+  Serial.println(curSpeed, DEC);    //This may print 'ovf, nan' if the data inside the EEPROM is not a valid float.
+  EEPROM.get(ADDRESS_MINPOS, minPos);
+  EEPROM.get(ADDRESS_MAXPOS, maxPos);
+  Serial.println("A");
+   
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  myMotor->setSpeed(curSpeed);  // 10 rpm   
+
+   // Setup callbacks for SerialCommand commands // Have to change define in serial command max # of commands to 20
+  SCmd.addCommand("move",move);       // move
+  SCmd.addCommand("speed",speed);       
+  SCmd.addCommand("pos?",reportPos);   
+  SCmd.addCommand("speed?",reportSpeed);    
+  SCmd.addCommand("setzero",setzero);    
+  SCmd.addCommand("stepsize",relStepSize);    
+  SCmd.addCommand("stepsize?",reportStepSize);  
+  SCmd.addCommand("home",home);  
+  SCmd.addCommand("setmax",setmax);
+  SCmd.addCommand("setmin",setmin);
+  SCmd.addCommand("poslim?",getPosLim);          
+  SCmd.addCommand("help",help);  
+          
+  SCmd.addDefaultHandler(unrecognized);  // Handler for command that isn't matched  (says "?") 
+
+  //setup the push buttons 
+  pinMode(kPinButton1, INPUT);
+  pinMode(kPinButton2, INPUT);
+  digitalWrite(kPinButton1, HIGH);
+  digitalWrite(kPinButton2, HIGH);
+
+  //setup the time check
+  timeCheck = millis();
+}
+
+void loop()
+{  
+  SCmd.readSerial();     // We don't do much, just process serial commands
+  if(digitalRead(kPinButton1)==LOW){
+     //Serial.println("Motor UP");
+     relmove(stepSize);
+     //delay(20);//optional delay if button pressed too much
+  }
+  if(digitalRead(kPinButton2)==LOW){
+     //Serial.println("Motor DOWN");
+     relmove(-1*stepSize);
+     //delay(20);//optional delay if button pressed too much
+  }
+
+  // update the eeprom every N s 
+  // since we dont update it during manual moves to avoid 
+  // too many eeprom writes
+  //this only updates the eeprom if value has actually changed
+  if (millis() - timeCheck > kEEPROM_update_time){
+    updateEEPROMPos();
+    timeCheck = millis();
+  }
+  
+  
+}
+
+void move(){
+  int pos;  
+  char *arg; 
+
+  arg = SCmd.next(); 
+  if (arg != NULL){
+    pos =atol(arg); 
+    moveMotor(pos);
+    updateEEPROMPos();
+    Serial.println("A"); 
+
+  } else{
+    Serial.println("?"); 
+  }
+}
+
+void moveMotor(int pos){
+    pos = constrain(pos,minPos,maxPos);// software hard limits to avoid motor crash
+    int nStep = pos-curPos;
+    if (nStep>=0){
+      myMotor->step(nStep, FORWARD, SINGLE); 
+      
+    }
+    else {
+      myMotor->step(-nStep, BACKWARD, SINGLE); 
+    }
+     
+    curPos=pos;
+}
+
+void updateEEPROMPos(){// update the eeprom, as long as its actually changed
+  if (curPos != eepromPos){
+    eepromPos = curPos;
+    EEPROM.put(ADDRESS_POS, eepromPos);  
+  }
+}
+
+
+// this is the move function for manual moves
+// doesnt update the eeprom as dont want to do too many writes
+// instead we have a function that checks every N s if we need to update the 
+// eeprom
+void relmove(int relStep){
+  int posFinal;
+  posFinal = curPos + relStep;
+  moveMotor(posFinal);
+}
+
+void reportPos(){ 
+  Serial.println(curPos);
+}
+
+void reportSpeed(){ 
+  Serial.println(curSpeed);
+}
+
+void speed(){
+  int s;  
+  char *arg; 
+
+  arg = SCmd.next(); 
+  if (arg != NULL){ 
+    s =atol(arg); 
+    myMotor->setSpeed(s);
+
+    curSpeed=s;
+    EEPROM.put(ADDRESS_SPEED, curSpeed);
+    Serial.println("A"); 
+  } 
+}
+
+void relStepSize(){
+  int s;  
+  char *arg; 
+
+  arg = SCmd.next(); 
+  if (arg != NULL){ 
+    s =atol(arg); 
+    stepSize = s;
+
+    Serial.println("A"); 
+  } 
+}
+void reportStepSize(){ 
+  Serial.println(stepSize);
+}
+void setzero(){// set current position as zero location
+  curPos=0;
+  EEPROM.put(ADDRESS_POS, curPos);
+  Serial.println("A"); 
+}
+
+void home(){
+  moveMotor(0);
+  Serial.println("A"); 
+}
+
+// This gets set as the default handler, and gets called when no other command matches. 
+void unrecognized()
+{
+  Serial.println("?"); 
+}
+
+void setmax(){
+  int s;  
+  char *arg; 
+
+  arg = SCmd.next(); 
+  if (arg != NULL){ 
+    s =atol(arg); 
+    maxPos = s;
+  } else  {
+    maxPos = curPos;
+  }
+  
+  EEPROM.put(ADDRESS_MAXPOS, maxPos);
+  Serial.println("A"); 
+}
+
+void getPosLim(){
+   Serial.println("Min pos:");
+   Serial.println(minPos); 
+   Serial.println("Max pos:");
+   Serial.println(maxPos);
+}
+
+void setmin(){
+  int s;  
+  char *arg; 
+
+  arg = SCmd.next(); 
+  if (arg != NULL){ 
+    s =atol(arg); 
+    minPos = s;
+  } else  {
+    minPos = curPos;
+  }
+  
+  EEPROM.put(ADDRESS_MINPOS, minPos);
+  Serial.println("A"); 
+}
+
+void help(){
+  Serial.println("Device commands:");
+  Serial.println("move val - moves device to val");
+  Serial.println("speed val - sets speed to val (valid #s 0 - 255)");
+  Serial.println("pos? - returns current device position");
+  Serial.println("speed? - return current device speed");
+  Serial.println("setzero - sets current position as zero");
+  Serial.println("stepsize");
+  Serial.println("stepsize?");
+  Serial.println("home");
+  Serial.println("setmax");
+  Serial.println("setmin");
+  Serial.println("poslim");
+}
+
